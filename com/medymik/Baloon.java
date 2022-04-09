8ed5c6b95fb6cf3e95274463b8993a70d4ed4fb7
medymik <mohammed.ymik@outlook.com>
2022-04-09 22:10:46 +0000

    add Baloon class

---
+++ com/medymik/Baloon.java
@@ -0,0 +1,22 @@
+package com.medymik;
+
+public class Baloon extends Aircraft implements Flyable {
+
+    private WeatherTower weatherTower;
+
+    Baloon(String name, Coordinates coordinates) {
+        super(name, coordinates);
+    }
+
+    @Override
+    public void updateConditions() {
+        
+    }
+
+    @Override
+    public void registerTower(WeatherTower weatherTower) {
+        this.weatherTower = weatherTower;
+        this.weatherTower.register(this);
+    }
+    
+}
