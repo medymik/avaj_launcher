95cf1d40310beacf6f893bbf50e248cb4a38e95c
medymik <mohammed.ymik@outlook.com>
2022-04-09 22:08:57 +0000

    add class Helicopter

---
+++ com/medymik/Helicopter.java
@@ -0,0 +1,22 @@
+package com.medymik;
+
+public class Helicopter extends Aircraft implements Flyable {
+
+    private WeatherTower weatherTower;
+
+    Helicopter(String name, Coordinates coordinates) {
+        super(name, coordinates);
+    }
+
+    @Override
+    public void updateConditions() {
+        
+    }
+
+    @Override
+    public void registerTower(WeatherTower weatherTower) {
+        this.weatherTower = weatherTower;
+        this.weatherTower.register(this);
+    }
+    
+}
