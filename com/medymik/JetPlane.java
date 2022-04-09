8da005e7031000ae54ddfdaf9c5a0d8951a3c441
medymik <mohammed.ymik@outlook.com>
2022-04-09 22:09:59 +0000

    add JetPlane class

---
+++ com/medymik/JetPlane.java
@@ -0,0 +1,22 @@
+package com.medymik;
+
+public class JetPlane extends Aircraft implements Flyable {
+
+    private WeatherTower weatherTower;
+
+    JetPlane(String name, Coordinates coordinates) {
+        super(name, coordinates);
+    }
+
+    @Override
+    public void updateConditions() {
+        
+    }
+
+    @Override
+    public void registerTower(WeatherTower weatherTower) {
+        this.weatherTower = weatherTower;
+        this.weatherTower.register(this);
+    }
+    
+}
