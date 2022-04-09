af75cd8c8c298be409944518af8aef4c666307ff
medymik <mohammed.ymik@outlook.com>
2022-04-09 21:36:35 +0000

    add Coordinates class

---
+++ com/medymik/Main.java
@@ -0,0 +1,9 @@
+package com.medymik;
+
+public class Main { 
+    public static void main(String[] args) {
+        // Coordinates cor = new Coordinates(12, 3, 19);
+
+        // System.out.println(cor.getHeight());
+    }
+}
